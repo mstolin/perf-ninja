4677d68ce000dacb9855a508ee627621e50b0429
Marcel <mstolin+github@posteo.de>
2025-06-09 13:02:44 +0200

    Solved warmup solution

--- labs/misc/warmup/solution.cpp
+++ labs/misc/warmup/solution.cpp
@@ -2,9 +2,11 @@
 #include "solution.h"
 
 int solution(int *arr, int N) {
-  int res = 0;
+  /*int res = 0;
   for (int i = 0; i < N; i++) {
     res += arr[i];
   }
-  return res;
+  return res;*/
+  N = (N * (N + 1)) / 2;
+  return N;
 }
